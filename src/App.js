4efdef964a785f07ab2055c5b12a70cde2c4441d
limbo96 <pooria.q.1996@gmail.com>
2024-07-17 15:18:58 +0330

    link nav bar to pages

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./pages/Shop";
 import LoginSignup from "./pages/LoginSignup";
 import Product from "./pages/Product";
-import ShopCategor from "./pages/ShopCategor";
+import ShopCategory from "./pages/ShopCategory";
 import Cart from "./pages/Cart";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
+        <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategor category="men" />} />
-        <Route path="/womens" element={<ShopCategor category="women" />} />
-        <Route path="/kids" element={<ShopCategor category="kid" />} />
+        <Route path="/mens" element={<ShopCategory category="men" />} />
+        <Route path="/womens" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kid" />} />
         <Route path="/product" element={<Product />}>
           <Route path=":productId" element={<Product />} />
         </Route>
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<LoginSignup />} />
+
+        </Routes>
       </BrowserRouter>
     </div>
   );
