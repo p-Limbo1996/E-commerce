f38abca9dc363aa384047689e20756e41ac82362
limbo96 <pooria.q.1996@gmail.com>
2024-07-17 14:54:08 +0330

    create navbar and styles && create pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
+import logo from "../assets/logo.png"
+import cart_icon from "../assets/cart_icon.png"
+
 const Navbar = () => {
+
+    const[menu,setMenu]=useState('shop')
+    
   return (
+
     <div className='navbar'>
 
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu('shop')}}>Shop {menu==='shop'?<hr />:<></>} </li>
+            <li onClick={()=>{setMenu('men')}}>Men {menu==='men'?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu('women')}}>Women {menu==='women'?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu('kids')}}>Kids {menu==='kids'?<hr />:<></>}</li>
+        </ul>
+
+        <div className="nav-login-card">
+           <button>Login</button>
+           <img src={cart_icon} alt="" />
+           <div className="nav-card-count">0</div>
+        </div>
+
     </div>
+
   )
 }
 
