c30c21749a96fa52a3c9aeed6c1bb9dec6970597
limbo96 <pooria.q.1996@gmail.com>
2024-07-19 15:13:32 +0330

    create populer folder and add in to the shop page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./Item.css"
-const Item = (props) => {
+const Item = ({id,name,image,new_price,old_price}) => {
   return (
     <div className='item'>
 
-        <img src={props.image} alt="" />
-        <p>{props.name}</p>
+        <img src={image} alt="" />
+        <p>{name}</p>
 
         <div className="item-prices">
-            <div className="item-price-new">{props.new_price}</div>
-            <div className="item-price-old">{props.old_price}</div>
+            <div className="item-price-old">${old_price}</div>
+            <div className="item-price-new">${new_price}</div>
         </div>
 
     </div>
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import { Populer } from '../Components/Populer/Populer'
 
 const Shop = () => {
   return (
     <div>
 
       <Hero/>
+      <Populer/>
     </div>
   )
 }
