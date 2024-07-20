06b5162a5205040044ec91a837625193aee101af
limbo96 <pooria.q.1996@gmail.com>
2024-07-20 18:02:50 +0330

    create login page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from "../Context/ShopContex";
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+
+ 
+  const {all_product}  = useContext(ShopContext);
+
+  const {productId}=useParams();
+
+
   return (
-    <div>Product</div>
+    <div className='product'>
+      Product
+      </div>
   )
 }
 
