c797256e6f4d9f7b1dde0e966407635596992d19
limbo96 <pooria.q.1996@gmail.com>
2024-07-20 14:45:00 +0330

    create ShopCategory and style

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import Product from "./pages/Product";
 import ShopCategory from "./pages/ShopCategory";
 import Cart from "./pages/Cart";
 import Footer from "./Components/Footer/Footer";
+import mens_banner from "./Components/assets/banner_mens.png"
+import womens_banner from "./Components/assets/banner_women.png"
+import kids_banner from "./Components/assets/banner_kids.png"
+
 const App = () => {
   return (
     <div>
       
       <BrowserRouter>
-        <Navbar />
+        <Navbar />  
         <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategory category="men" />} />
-        <Route path="/womens" element={<ShopCategory category="women" />} />
-        <Route path="/kids" element={<ShopCategory category="kid" />} />
+        <Route path="/mens" element={<ShopCategory baner={mens_banner}   category="men" />} />
+        <Route path="/womens" element={<ShopCategory  baner={womens_banner} category="women" />} />
+        <Route path="/kids" element={<ShopCategory baner={kids_banner}  category="kid" />} />
         <Route path="/product" element={<Product />}>
           <Route path=":productId" element={<Product />} />
         </Route>
